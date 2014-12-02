715e6e38939b749c56be7d230536e4d1742b5683
maubar <mauriciobarr@gmail.com>
2014-12-02 19:14:25 +0100

    New version of the blastxml hitsort that handles both files and folders and does parallel processing of the samples using multiprocessing

---
+++ python/parallel_blastxml_hitsort.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+import sys
+import argparse
+import os.path
+
+#Time of script execution and logging module
+# import time
+# import logging
+import re
+import itertools
+# from collections import *
+# import random
+
+import multiprocessing
+from Bio.Blast import NCBIXML
+
+
+def main():
+	args = create_argparser()
+	files_to_process = get_files_to_process(args)
+	process_pool = multiprocessing.Pool(args.num_proc)
+	hits_per_file = process_pool.map(blastxml_parser, files_to_process)
+	merged_hits = merge_and_sort_hits( hits_per_file )
+	write_hits_to_file(merged_hits,args.output_file)
+
+
+def blastxml_parser_factory(args):
+	eval_threshold = args.threshold
+	include_alignment = args.alignments
+	max_hits = args.max_hits
+
+	def _blastxml_parser(blast_filename):
+		hits = []
+		with open(blast_filename,"r") as blast_fh:
+			iterations = NCBIXML.parse(blast_fh)
+			for iteration in iterations:
+				iteration_hits = process_iteration(iteration, eval_threshold, blast_filename, include_alignment, max_hits)
+				if iteration_hits:
+					hits += iteration_hits
+		return hits
+	return _blastxml_parser
+
+def process_iteration(iteration_obj,eval_threshold, source_file, include_alignment=False,max_hits=1):
+	iteration_hits = []
+	for hit_num,hit in enumerate(iteration_obj.alignments):
+		hsp = hit.hsps[0] #Only first hsp
+		#Store relevant fields in a tuple
+		if hsp.expect < eval_threshold:
+			visual_alignment = None
+			if include_alignment:
+				visual_alignment = ( hsp.sbjct_start, hsp.sbjct, hsp.sbjct_end, hsp.match, hsp.query_start,hsp.query,hsp.query_end )
+			species = extract_species_name(hit.hit_def)
+			pct_id, pct_pos, q_cov = calc_hsp_stats(hsp, iteration_obj.query_length)
+			result = (hsp.expect,
+					species,
+					iteration_obj.query_length,
+					q_cov,
+					pct_id,
+					pct_pos,
+					iteration_obj.query,
+					source_file,
+					hit.hit_def,
+					visual_alignment)
+			iteration_hits.append(result)
+
+		#Limit number of hits to report
+		if hit_num >= (max_hits-1):
+			break
+	return iteration_hits
+
+#Ideally annotate from TaxID
+def extract_species_name(hit_name):
+	species = hit_name
+	blastx_species = re.search(r"\[(.+?)\]",species)
+	if blastx_species:
+		species = blastx_species.group(1)
+	else:
+		species = hit_name.split(",")[0]
+	return species
+
+#Calculates percent identity, percent positives and query coverage
+def calc_hsp_stats( hsp , qseq_len):
+	pct_id = 100.0 * hsp.identities / hsp.align_length
+	pct_pos = 100.0 * hsp.positives / hsp.align_length
+	q_cov = 100.0 * (hsp.query_end - hsp.query_start) / qseq_len
+	return pct_id, pct_pos, q_cov
+
+def merge_and_sort_hits(hits_per_file):
+	combined_hits = [x for x in itertools.chain(hits_per_file)]
+	combined_hits.sort(key=lambda item:item[0],reverse=False)
+	return combined_hits
+
+def write_hits_to_file(hits,out_fh,write_alignments=False):
+	output_columns = ["e-value", "species", "seq_len", "q_cov","pct_id","pct_pos","ctg_id","other"]
+	out_fh.write("\t".join(output_columns)+"\n")
+	for res in hits:
+		out_fh.write( "\t".join([str(x) for x in res[:8]])+"\n")
+		if write_alignments:
+			#print alignments as well
+			alignment = res[8]
+			out_fh.write( "\n"+formatAlignment(alignment)+"\n\n" )
+
+# Alignment receives a tuple with the following fields:
+# hsp.sbjct_start, hsp.sbjct, hsp.sbjct_end, hsp.match, hsp.query_start,hsp.query,hsp.query_end
+def formatAlignment(alignment,space_padding="   "):
+	pos_padding = max(len(alignment[0]), len(alignment[4]) )
+
+	ref_line = space_padding + "   Ref: {: <"+ str(pos_padding) + "}{}".format( alignment[0] , space_padding )
+	qry_line = space_padding + "   Qry: {: <"+ str(pos_padding) + "}{}".format( alignment[4] , space_padding )
+	midline = " " * len(ref_line)
+
+	#Add sequences , padding and end coordinates
+	ref_line += alignment[1] + space_padding + str(alignment[2])
+	midline  += alignment[3]
+	qry_line += alignment[5] + space_padding + str(alignment[6])
+
+	return ref_line+"\n"+midline+"\n"+qry_line
+
+#******************************************************************************
+#			Console argument processing
+#******************************************************************************
+
+#Assumes files and folder have been validated
+def get_files_to_process(args):
+	files_to_process = []
+	for current_file in filter(os.path.isfile, args.input):
+		files_to_process.append(current_file)
+	for folder in filter(os.path.isdir, args.input):
+		for root_folder,dirs,files in os.walk(folder):
+				files_to_process += [os.path.join(root_folder,f) for f in files if f.endswith(".xml") and "blast" in f ]
+	#Filters
+	if args.filter:
+		files_to_process = filter(lambda x:args.filter in x, files_to_process)
+	if args.filter_out:
+		files_to_process = filter(lambda x: args.filter_out not in x, files_to_process)
+
+	return files_to_process
+
+def create_argparser():
+	parser = argparse.ArgumentParser(description="The program extracts the highest scoring hits for each query \
+		sequence and outputs them ordered by e-value. Requires filenames to finish in _tool_blast[nxp]_database.xml")
+
+	parser.add_argument("input",nargs="+",help="Folder in which to look for blast xml files")
+	parser.add_argument("-p","--num_proc", type=int, default=1, help="Number of processes to parse the files concurrently. There must be enough memory to keep both in memory")
+	parser.add_argument("-o","--output-file", type=argparse.FileType('w'), default=sys.stdout, help="Name of the output file" )
+	parser.add_argument("-l","--log-file", default=None, help="Name of the log file")
+
+	parser.add_argument("--max_hits",type=int,default="10",help="Max number of hits to report for each query sequence")
+	parser.add_argument("-a","--alignments",action='store_true',help="Include a visualization for each alignment on the file")
+	parser.add_argument("-t","--threshold",type=float,default=1,help="Exclude hits with e-value lower than threshold. Default is '1' (all hits)")
+
+	parser.add_argument("-f","--filter",type=str,help="Only process files that contain the filter string")
+	parser.add_argument("-F","--filter-out",type=str,help="Only process files that DO NOT contain the filter string")
+
+	return parser.parse_args()
+
+
+if __name__ == '__main__':
+	main()
